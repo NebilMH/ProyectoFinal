1d33b0e0b48a33ba2d90912c2f44b3f4642688d0
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-11 21:58:49 +0200

    Delete a directory

--- a/proyecto/php/pago.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $array = $_POST['values_array'];
-
-    echo "Valor recogido: " . $array . "<br/>";
-
-    echo "Convertirlo en array: ";
-    print_r(explode(",", $array));
-?>
--- a/proyecto/php/verificar-usuario.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include('conexionBD.php');
-
-    if ((isset($_SESSION['usuario'])) && ($_SESSION['usuario'] != "")) {
-        echo "Bienvenido/a ". $_SESSION['usuario'];
-    } else {
-        header("refresh:0;url=login.php");
-    }
-?>
