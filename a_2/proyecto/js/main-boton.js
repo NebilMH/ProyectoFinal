0f129d5d158143b0a7c08cee4a5aedb03c1d96d1
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-11 21:59:56 +0200

    Delete a_2 directory

--- a_2/proyecto/js/main-boton.js
+++
@@ -1,6 +0,0 @@
-const btnSwitch = document.querySelector('#switch');//Este script srive para alternar el modo oscuro de las paginas con el boton de modo oscuro
-
-btnSwitch.addEventListener('click', () => {
-	document.body.classList.toggle('dark');
-	btnSwitch.classList.toggle('active');
-});
