33637ede9034047d0c0c784df8efef29a6f6d70d
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-13 05:54:50 +0200

    Delete a directory

--- a/proyecto/php/despliegue/instalador.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try {
-        shell_exec("/var/www/html/ProyectoFinal/a/proyecto/php/despliegue/instalador.sh");
-        echo "<script type='text/javascript'>window.location.href = 'instalacion-correcta.php';</script>";
-    } catch (\Throwable $th){
-        throw $th;
-    }
-?>
--- a/proyecto/php/vaciarCarrito.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['datos']);//Le especificamos la sesion que queremos que cierre para no cerrar todas
-
-    echo "<script type='text/javascript'>window.location.href = 'carrito.php';</script>";
-?>
