f91f4a07511624b89445e153d8817cd70592f709
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-13 14:32:48 +0200

    Update instalador.php

--- a/proyecto/php/despliegue/instalador.php
+++ a/proyecto/php/despliegue/instalador.php
@@ -1,4 +1,14 @@
 <?php
+    $bd = $_POST['bd'];
+    $usuario = $_POST['usuario'];
+    $contrasenia = $_POST['contrasenia'];
+
+    putenv("BBDD=$bd");
+    putenv("USERDB=$usuario");
+    putenv("PASSDB=$contrasenia");
+
+    $output = exec('instalador.sh');
+
     try {
         shell_exec("/var/www/html/ProyectoFinal/a/proyecto/php/despliegue/instalador.sh");
         echo "<script type='text/javascript'>window.location.href = 'instalacion-correcta.php';</script>";
