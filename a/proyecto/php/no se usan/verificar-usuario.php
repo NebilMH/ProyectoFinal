15084b016dac7db8d70349402318c617abc5216e
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-13 20:56:40 +0200

    Delete a/proyecto directory

--- a/proyecto/php/no se usan/verificar-usuario.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include('conexionBD.php');
-
-    if ((isset($_SESSION['usuario'])) && ($_SESSION['usuario'] != "")) {
-        echo "Bienvenido/a ". $_SESSION['usuario'];
-    } else {
-        echo "<script type='text/javascript'>window.location.href = 'login.php';</script>";
-    }
-?>
