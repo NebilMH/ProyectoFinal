289f6ee9d17f59a5b568e0639192e5624bb9b405
NebilMH <93333103+NebilMH@users.noreply.github.com>
2022-06-17 22:36:37 +0200

    Delete a directory

--- a/proyecto/js/ejercicio.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-bar'); //Seleccionamos el elemento que contenga dicha id y lo almacenamos
-let navbar = document.querySelector('.navbar');
-
-menu.onclick =() =>{ //Permite alternar el menu desplegable que aparece cuando la resolucion es pequeña
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
